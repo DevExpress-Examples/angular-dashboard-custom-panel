3fddc22b7966cdce9aa248346339f80070a13a9d
Polina Tyureva <polina.tyureva@devexpress.com>
2024-01-04 20:37:13 +0400

    Update to Angular 17

--- dashboard-angular-app/src/app/app.component.ts
+++ dashboard-angular-app/src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { DxDashboardControlModule } from 'devexpress-dashboard-angular';
+import { DevExtremeModule } from 'devextreme-angular';
 import { createStore } from 'devextreme-aspnet-data-nojquery';
 import CustomStore from 'devextreme/data/custom_store';
 import DataSource from 'devextreme/data/data_source';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, DxDashboardControlModule, DevExtremeModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  serverUrl: string = "http://localhost:5000";
+  serverUrl: string = "https://localhost:5001";
   /* To debug the server, change 'serverUrl' to https://localhost:44396 and run the server application in Visual Studio. */
   store: CustomStore;
   dataSource: DataSource;
